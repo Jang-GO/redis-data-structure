95d09a993209d8981742603da2fecd88937e284d
Jang-GO <jang8195@naver.com>
2025-09-02 20:30:41 +0900

    feat: RedisStringService 및 테스트 작성

--- src/test/java/com/example/redisdatastructure/string/RedisStringServiceTest.java
+++ src/test/java/com/example/redisdatastructure/string/RedisStringServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 import java.util.List;
